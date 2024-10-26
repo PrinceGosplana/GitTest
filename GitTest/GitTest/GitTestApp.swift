a23e8d49d097867d01a496f08bf1a9ce7b2b61e3
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-26 20:58:12 +0300

    init

---
+++ GitTest/GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by OLEKSANDR ISAIEV on 26.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitTest/GitTestTests/GitTestTests.swift
@@ -0,0 +1,16 @@
+//
+//  GitTestTests.swift
+//  GitTestTests
+//
+//  Created by OLEKSANDR ISAIEV on 26.10.2024.
+//
+
+import Testing
+
+struct GitTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
