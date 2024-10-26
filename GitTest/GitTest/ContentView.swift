53a75b3126b7565b2afed0b36898b8341e502b2f
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-26 21:00:44 +0300

    add values

--- GitTest/GitTest/ContentView.swift
+++ GitTest/GitTest/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var value = 75.0
+    private let minValue = 0.0
+    private let maxValue = 100.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 50.0) {
+            Gauge(value: value) {
+                Text("No Range")
+            }
+            Gauge(value: value, in: minValue...maxValue) {
+                Text("Range")
+            }
         }
-        .padding()
     }
 }
 
