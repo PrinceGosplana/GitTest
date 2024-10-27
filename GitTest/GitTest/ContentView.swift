6441616397bc06bea6fd7b92340b767be146adbe
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-27 20:51:14 +0200

    add gradiented element

--- GitTest/GitTest/ContentView.swift
+++ GitTest/GitTest/ContentView.swift
@@ -13,11 +13,21 @@ struct ContentView: View {
     private let minValue = 0.0
     private let maxValue = 100.0
     
+    let gradient = Gradient(colors: [.blue, .yellow, .orange, .red])
+    
     var body: some View {
         VStack(spacing: 50.0) {
-            Gauge(value: value) {
-                Text("No Range")
+            Gauge(value: value, in: minValue...maxValue) {
+                Label("Range", systemImage: "arrow.left.and.right")
+            } currentValueLabel: {
+                Text(value, format: .number)
+            } minimumValueLabel: {
+                Text(minValue, format: .number)
+            } maximumValueLabel: {
+                Text(maxValue, format: .number)
             }
+            .tint(gradient) // Doesn't affect labels
+            
             Gauge(value: value, in: minValue...maxValue) {
                 Text("Range")
             }
